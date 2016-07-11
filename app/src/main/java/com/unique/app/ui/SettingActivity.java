cefab038a1a3f31070a5d79de8e0a88877f1346b
Crainax <413997411@qq.com>
2016-07-11 13:56:25 +0800

    me activity UI done

---
+++ app/src/main/java/com/unique/app/ui/SettingActivity.java
@@ -0,0 +1,14 @@
+package com.unique.app.ui;
+
+/**
+ * Project: CommnitiAPP <br/>
+ * Package: com.unique.app.ui <br/>
+ * Description: <br/>
+ * <hr/>
+ *
+ * @author Crainax <br/>
+ * @version 1.0 <br/>
+ * @since 2016/7/11 <br/>
+ */
+public class SettingActivity extends BaseActivity{
+}
